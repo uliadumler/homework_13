296ba0f20654d67a4264717ec4d018ce4cd56618
Ulia Dumler <uliadumler@gmail.com>
2018-06-24 10:07:35 +0400

    Основное дз 13 исправления

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
 $(document).ready(function() {
 
 	function start() {
-		$('.overlay').fadeTo(500, 1);
+		$('.overlay').fadeIn('slow');
 		$('.modal').slideDown('slow');
 	}
 
-	$('.main_btna').on('click', function() {
-		start();
-	});
-
-	$('.main_btn').on('click', function() {
-		start();
-	});
-
-	$('div.main_nav > nav > ul > li:nth-child(2)').on('click', function() {
+	$('.main_btna, .main_btn, li a[href$=sheldure]').on('click', function() {
 		start();
 	});
 
 	function close() {
-		$('.overlay').fadeTo(500, 0);
+		$('.overlay').fadeOut('slow');
 		$('.modal').slideUp('slow');
 	}
 
 	$('button.close > span').on('click',function() {
 		close();
 	});
+
+	var each = function(startArr, f) {
+		return f(startArr);
+	};
+
+	var arr = [64, 49, 36, 25, 16];
+
+	var myFunc = function(a) {
+		var newArr = [];
+		for (var i = 0; i < a.length; i++) {
+			newArr[i]=Math.sqrt(a[i]);
+		}
+		return newArr;
+	}
+
+	console.log(each(arr, myFunc));
+
+	console.log(typeof(arr));
 });
+
